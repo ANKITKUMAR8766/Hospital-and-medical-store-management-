670adf9e817922dd282bf3c52d8d5b04366dd67a
ANKIT KUMAR <145970942+ANKITKUMAR8766@users.noreply.github.com>
2025-04-01 02:18:37 +0530

    Create 1.function

---
+++ 1.c
@@ -0,0 +1,15 @@
+// Structure for Hospital 
+struct Hospital { 
+	char name[50]; 
+	char city[50]; 
+	int beds; 
+	float price; 
+	float rating; 
+	int reviews; 
+}; 
+
+//Structure for Patient 
+struct Patient { 
+	char name[50]; 
+	int age; 
+};
